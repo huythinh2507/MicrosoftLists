63a723047eb5520796a58b56f566a10eb655af1d
huythinh2507 <74760682+huythinh2507@users.noreply.github.com>
2024-07-25 11:18:39 +0700

    reset

--- DataLayer/MsLConstant.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataLayer
-{
-    public static class MsLConstant
-    {
-        public const int DefaultColWidth = 10;
-        public const string FilePath = "C:\\Users\\thinh\\source\\repos\\MicrosoftLists\\templates.json";
-        public const int WidthIncrement = 2;
-    }
-}
--- DataLayer/View.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DataLayer
-{
-    public class View
-    {
-        public ViewType Type { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    }
-}
